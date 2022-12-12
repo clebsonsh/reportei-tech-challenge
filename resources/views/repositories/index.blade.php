7584c56de654eeace7cef5fa3e72434d3adecbff
Clebson Moura <clebson.sh@gmail.com>
2022-12-12 11:48:41 -0300

    logout button added

--- resources/views/repositories/index.blade.php
+++ resources/views/repositories/index.blade.php
@@ -1,4 +1,17 @@
 <x-app-layout>
+    <div class="flex justify-end">
+        @auth
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+
+                <x-dropdown-link :href="route('logout')"
+                    onclick="event.preventDefault();
+                                this.closest('form').submit();">
+                    {{ __('Log Out') }}
+                </x-dropdown-link>
+            </form>
+        @endauth
+    </div>
     <div class="flex justify-center">
         <div class="text-white">
             @foreach ($repositories as $repository)
